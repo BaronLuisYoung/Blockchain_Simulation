2ca4f551671348fb9bf1d86eceb7cc606b197d54
Baron Young <baronyoung@ecellvm-01.engr.ucsb.edu>
2023-04-19 13:56:37 -0700

    Passes testcases

--- client.py
+++ client.py
@@ -14,91 +14,54 @@ def wait(t):
 
 def exit():
     out_sock.close()
-    #flush console ouput buffer in case there are remaining prints
     stdout.flush()
     os._exit(0)
 
 def get_user_input():
-    #print("Please input user ID:")
     user_input_id = sys.argv
-    sleep(int(sys.argv[1]))
     user_input_id = "P" + user_input_id[1]
     try:
-        #send user input string to server, converted into bytes
         out_sock.sendall(bytes(user_input_id, "utf-8"))
-        #handling exception in case trying to send data to closed connection 
     except:
         print("exception in sending to server")
-        
 
-    #print("Please input transaction or query current balance:")
     while True:
         user_input = input()
+        #print(user_input)
         if user_input.split()[0] == "exit":
             exit()
         elif user_input.split()[0] == "wait":
             wait(int(user_input.split()[1]))
         else:
             try:
-                #send user input string to server, converted into bytes
-                out_sock.sendall(bytes(user_input, "utf-8"))
-                #handling exception in case trying to send data to closed connection 
+                out_sock.sendall(bytes(user_input, "utf-8")) 
             except:
-                #print("exception in sending to server")
                 continue
-        #print("sent latest input to server")
-
-def handle_msg(data):
-    #sleep(3)
-    #decode data into string 
-    data = data.decode()
-    print(data)
 
 if __name__ == "__main__":
-    sleep(1)
-    #specify servers socket address so client can connect to it 
-    #client and server are just different processes on the same machine
-    #server's IP is just local machine's IP
+    sleep(1) #for autograder
+
     SERVER_IP = socket.gethostname()
-    SERVER_PORT = 9000
-
-    #create a socket object, SOCK_STREAM specifies a TCP socket
-    #do not need to specify address for own socket for making an outboud connection
+    SERVER_PORT = 8000
     out_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    #attempt to connect own socket to server's socket address 
     
-    while True:
-        try:
-            out_sock.connect((SERVER_IP, SERVER_PORT))
-            break
-        except:
-            #print(f"connection failed on address: {SERVER_IP}, port: {SERVER_PORT}\n")
-            sleep(2)
-            #print("attempting to connect ... \n")
-            continue
-    #print("sucess, connected to server")
+    out_sock.connect((SERVER_IP, SERVER_PORT))
+           
 
-    #spawn new thread to keep waiting for user inputs 
-    #so user input and socket receive do not block eachother
+    #thrd. for user inputs 
     threading.Thread(target=get_user_input).start()
 
-    #infinite loop to keep waiting to receive new data from server 
+    #wait receive data from server 
     while True:
         try:
-        #wait to recive new data, 1024
             data = out_sock.recv(1024)
-            print(f"{data.decode()}")
-    #handle exception in case connection failure
+            str_data = str(data.decode())
+            if str_data:
+                print(str_data, flush=True)
         except:
-            #print("exception in receiving")
-            break 
+            continue
         if not data:
-            #close own socket since other end closed
-            out_sock.close()
-            #print("connection close from server")
+            out_sock.close() #close own socket since other end closed
             break
-
-    #spawn a new thread to handle message 
-    # so simulated network delay and message handling dont block receive
-    threading.Thread(target=handle_msg, args=(data,)).start()
+  
     
--- server.py
+++ server.py
@@ -14,8 +14,8 @@ out_socks = [] #container for all client connections
 active_clients = {} #[port] = id
 Blockchain = Blockchain() #locally defined block chain
 lock = threading.Lock() #lock used for adding block
-#_____________________________________________________________________________#
 
+#___________________________FOR SERVER CMDs___________________________________#
 def wait(t):
     sleep(t)
 
@@ -23,25 +23,22 @@ def exit():
     in_sock.close()
     for sock in out_socks:
         sock[0].close()
-    stdout.flush() #from sys lib
-    #exit w/ status 0 
-    os._exit(0) #imported from os lib
+    stdout.flush() 
+    os._exit(0) 
+#_____________________________________________________________________________#
 
 def send_msg_to_client(port, data):
     try:
-        #convert message into bytes and send through socket
         port.sendall(bytes(f"{data}", "utf-8"))
-        print(f"sent message to port {port}", flush=True)
-        #handling exception in case trying to send data to a closed connection
     except:
         print(f"exception in sending port {port}", flush=True)
 
-def process_transfer_request(port, recipient_id, amnt): #sender, recv, amnt 
-    new_balance = Blockchain.check_transfer(active_clients[port], int(amnt[1:]))
+def process_transfer_request(port, recipient_id, amnt): # T format: <Sender, Reciever-ID, amnt>
+    
+    new_balance = Blockchain.check_balance(active_clients[port], int(amnt[1:])) #returns -1 if invalid 
+   
     if new_balance != -1: 
-        #create block and add it to chain
-        #print(amnt[1:])
-        trans = (active_clients[port], recipient_id, amnt[1:]) # Trasaction of format <Sender, Reciever, amount>
+        trans = (active_clients[port], recipient_id, amnt[1:]) #(S-ID, R-ID, Amnt)
         Blockchain.add_block(trans)
         return "Success"
     else:
@@ -49,34 +46,28 @@ def process_transfer_request(port, recipient_id, amnt): #sender, recv, amnt
 
 def get_user_input():
     while True:
-        lock.acquire()
         user_input = input()
+        #print(user_input)
         if user_input == "Blockchain":
-            print(Blockchain.print_chain(), flush=True)  
+            #lock.acquire()
+            chain_str = Blockchain.print_chain()
+            print(chain_str)  
+            #lock.release()
         elif user_input == "exit":
             exit()
         elif user_input.split()[0] == "wait":
-            wait(int(user_input.split()[1]))
+            wait(int(user_input.split()[1])) # gets time arg in cmd: 'wait x', x = time
         elif user_input == "Balance":
-            data = "" 
-            for i in range(1,3,1):
-                data = data + "P" + str(i) + ": $" + str(Blockchain.check_balance("P" + str(i))) + ", "
-            print(data.strip()[:-1].strip())
-            #imlement for other request that fail
+            #lock.acquire()
+            balance_str = "" 
+            for i in range(1,4,1):
+                balance_str = balance_str + "P" + str(i) + ": $" + str(Blockchain.check_balance("P" + str(i), 0)) + ", "
+            print(balance_str.strip()[:-1]) #removes last comma 
+            #lock.release()
         else:
-            continue
-        lock.release()
+            continue #ignore other inputs and continue
 
-
-def init_client_id(client_id, port):
-   #print(port, client_id)
-   active_clients[port] = client_id.decode("utf-8")
-   #print(f"New client ID: {active_clients[port]} added on port {port}")
-
-#simluates newtork delay then handles recieved message 
 def handle_msg(data, addr):
-    #simulate 3 seconds message-passing delay
-    #sleep(3)#imported from time library
     data = data.decode() #decode byte data into a string
 
     client_request = data.split() 
@@ -86,116 +77,66 @@ def handle_msg(data, addr):
             #send back failed request
             data = "Failed Request, client cannot send money to themselves."
         else:
-            lock.acquire()
+            #lock.acquire()
             data = process_transfer_request(addr[1], client_request[1], client_request[2]) #passes: <port, recv client, amnt>
-            lock.release()
-    elif client_request[0] == "Balance" and len(client_request) > 1: #process a balance request
-        data = "Balance: $" + str(Blockchain.check_balance(client_request[1]))#requester 
-    elif client_request[0] == "Blockchain":
-        data = Blockchain.print_chain()
+            #lock.release()
+    elif client_request[0] == "Balance": #process a balance request
+        #lock.acquire()
+        data = "Balance: $" + str(Blockchain.check_balance(client_request[1], 0))
+        #lock.release()
     else:
-        pass
-    #sends back message to console 
-    #print(f"{addr[1]}: {data}", flush=True)
-    #bcast to all clients by iterating through stored connections
-    for sock in out_socks:
-        #conn = sock[0]
-        #print(conn)
-        #recv_addr = sock[1]
-        #echo message back to client
-        if addr[1] == sock[1][1]:
-            try:
-                #convert message into bytes and send through socket
-                conn = sock[0]
-                conn.sendall(bytes(f"{data}", "utf-8"))
-                #print(f"sent message to port {recv_addr[1]}", flush=True)
-                #handling exception in case trying to send data to a closed connection
-            except:
-                #print(f"exception in sending port {recv_addr[1]}", flush=True)
-                continue
-   
-#handles a new connection by waiting to recieve from connection 
+        return
+        #invalid input send back nothing
+
+    for sock in out_socks: #sends back message to console 
+        if addr == sock[1]:
+            send_msg_to_client(sock[0], data)
+
+ 
 def respond(conn, addr):
-    #print(f"accepted connection from port: {addr[1]}", flush=True)
     try:
-        #wait to reveive new data, 1024 is receive buffer size
-        client_id_data = conn.recv(1024) #may need to be smaller
+        client_id_data = conn.recv(1024)
     except:
-        #handle exception in case something happened to connection but it's not properly closed 
         print(f"exception in user ID receving from {addr[1]}", flush=True)
     
-    init_client_id(client_id_data, addr[1])
-    #infinite loop to keep waiting to receive new data from this client
+    active_clients[addr[1]] = client_id_data.decode("utf-8") # add client to active clients
     while True:
         try:
-            #wait to reveive new data, 1024 is receive buffer size
-            data = conn.recv(1024) #may need to be larger 
+            data = conn.recv(1024) 
         except:
-            #handle exception in case something happened to connection
-            #but it's not properly closed 
-            print(f"exception in receving from {addr[1]}", flush=True)
-            break
+            break #exception in receving from addr[1]
 
-        #if cilents socket closed it will signal closing without any data 
+        #if cilents socket closed
         if not data:
-            #close own sock since client end closed
             conn.close()
-            #print(f"connection closed from {addr[1]}", flush=True)
             break 
-        #else we spawn a new thread to handle message sent from client
-        #so the simulated network delay and message handling don't block receive
+        
+        #thread handles msg from client
         threading.Thread(target=handle_msg, args=(data, addr)).start()
 
 if __name__ == "__main__":
-    IP = socket.gethostname() #gets the IP (address) of local machine 
-    PORT = 9000 #3000-49151 are genreally usable 
+    IP = socket.gethostname() 
+    PORT = 8000 #3000-49151 are genreally usable 
 
-    #creates socket object
-    #AF_INET addr. family (used IPV4), SOCK_STREAM specifies TCP communication 
     in_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    #sock ramains open after shutdown (1-2mins), TIME-WAIT state
-    #set REUSEADDR to disable "socket already in use"
     in_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
     in_sock.bind((IP, PORT)) #bind socket to the specified addrs
 
     in_sock.listen() #begin accepting connections 
 
-    #spawn thread to wait for user input (this will terminate server)
+    #thrd. for user input 
     threading.Thread(target=get_user_input).start()
 
     while True:
-        # wait to accept any incoming connections
-		# conn: socket object used to send to and receive from connection
-		# addr: (IP, port) of connection 
         try:
           conn, addr = in_sock.accept()
         except:
           print("exception in accept", flush=True)
           break
-        #add client connection to list 
         out_socks.append((conn, addr))
-        #spawn a thread for each client connection 
+        #thrd. for each client
         threading.Thread(target=respond, args=(conn,addr)).start()
-    
        
     
-
-'''
-Genesis block 
-use 64 zeros for 
-
-both server and client will need wait and sleep cmd functions 
-
-add genesis block only when we add the first block
-can we print to console 
-
-
-
-genesis block contents?
-need has with the transaction 
-
-
-'''
-
